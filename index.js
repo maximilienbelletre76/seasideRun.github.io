a25f0ad681afa270246ab56404db448f4806ce30
BELLETRE Maximilien <mbelletre@toshiba-teis.com>
2025-11-19 12:22:28 +0100

    ajout animation js avec le menu + ajout lien vers calendrier et notre club , lien vers acceuil

--- index.js
+++ index.js
@@ -1,29 +1,66 @@
 // aficher le menu
-
 const logo = document.querySelector("#Home");
 const menu = document.querySelector("#menuContainer");
+let menuOpen = false;
 
 function showMenu() {
   menu.style.visibility = "visible";
   menu.style.opacity = "0.8";
+  menuOpen = true;
 }
 
 function hideMenu() {
   menu.style.visibility = "hidden";
   menu.style.opacity = "0";
+  menuOpen = false;
 }
 
-logo.addEventListener("mouseenter", showMenu);
-logo.addEventListener("mouseleave", () => {
-  setTimeout(() => {
-    if (!menu.matches(":hover")) hideMenu();
-  }, 100);
+// clic sur logo
+logo.addEventListener("click", function (e) {
+  if (!menuOpen) {
+    e.preventDefault();
+    showMenu();
+  } else {
+    hideMenu();
+  }
 });
 
-menu.addEventListener("mouseleave", hideMenu);
+// empêcher fermeture si clic dans le menu
+menu.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
 
-// animation image box
+// fermer menu si clic en dehors
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && e.target !== logo) {
+    hideMenu();
+  }
+});
 
+//logo remonte en cas d'inactivité
+let inactivityTimer;
+
+function setActive() {
+  logo.classList.remove("inactive");
+  resetTimer();
+}
+
+function setInactive() {
+  logo.classList.add("inactive");
+}
+
+function resetTimer() {
+  clearTimeout(inactivityTimer);
+  inactivityTimer = setTimeout(setInactive, 1500); // 3 secondes d'inactivité
+}
+
+// Événements pour détecter l'activité de l'utilisateur
+document.addEventListener("mousemove", setActive);
+document.addEventListener("keydown", setActive);
+document.addEventListener("touchstart", setActive);
+
+// lancer le timer au départ
+resetTimer();
 const imgBox = document.querySelector("#imgbox");
 //generer les images
 for (let i = 1; i <= 20; i++) {
